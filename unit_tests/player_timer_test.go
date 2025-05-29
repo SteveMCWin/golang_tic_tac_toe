329508afc11962d249967dedc672c56eb7a2edd1
SteveMCWin <stevp003@gmail.com>
2025-05-29 04:19:34 +0200

    changed some small stuff, such as lazy initializing the db connection

---
+++ unit_tests/player_timer_test.go
@@ -0,0 +1,95 @@
+package tests
+
+import (
+    "testing"
+    "time"
+    "tic_tac_toe.fun/game"
+)
+
+func TestTimerRunsOut(t *testing.T) {
+    t.Parallel()
+	pt := game.MakePlayerTimer(1 * time.Second)
+	pt.Start()
+
+	select {
+	case <-pt.Finished:
+	case <-time.After(2 * time.Second):
+		t.Error("Timer didn't finish on time!")
+	}
+}
+
+func TestTimerPause(t *testing.T) {
+    t.Parallel()
+	pt := game.MakePlayerTimer(1 * time.Second)
+	pt.Start()
+
+    time.Sleep(50 * time.Millisecond)
+
+    pt.Pause()
+
+	select {
+	case <-pt.Finished:
+		t.Error("Timer finished even when stopped!")
+	case <-time.After(2 * time.Second):
+	}
+}
+
+func TestTimerRace(t *testing.T) {
+    t.Parallel()
+    pt1 := game.MakePlayerTimer(3 * time.Second)
+    pt2 := game.MakePlayerTimer(3 * time.Second)
+
+    pt1.Start()
+
+    time.Sleep(1 * time.Second)
+    pt2.Start()
+
+    select {
+    case <-pt1.Finished:
+        return
+    case <-pt2.Finished:
+        t.Error("Timer 2 finished efore timer 1")
+    }
+
+}
+
+func TestTimerRace2(t *testing.T) {
+    t.Parallel()
+    pt1 := game.MakePlayerTimer(3 * time.Second)
+    pt2 := game.MakePlayerTimer(3 * time.Second)
+
+    pt1.Start()
+
+    time.Sleep(1 * time.Second)
+    pt1.Pause()
+    pt2.Start()
+
+    select {
+    case <-pt1.Finished:
+        t.Error("Timer 1 finished efore timer 2")
+    case <-pt2.Finished:
+        return
+    }
+
+}
+
+func TestPauseAccuracy(t *testing.T) {
+    t.Parallel()
+    delta := 20 * time.Millisecond
+
+    pt1 := game.MakePlayerTimer(3 * time.Second)
+    pt1.Start()
+    time.Sleep(1000 * time.Millisecond)
+    pt1.Pause()
+    if (pt1.TimeLeft - 2000 * time.Millisecond).Abs() > delta {
+        t.Error("Timer unprecise. Expected about 2000ms, got", pt1.TimeLeft)
+    }
+    time.Sleep(200 * time.Millisecond)
+    pt1.Start()
+    time.Sleep(1000 * time.Millisecond)
+    pt1.Pause()
+    if (pt1.TimeLeft - 1000 * time.Millisecond).Abs() > delta {
+        t.Error("Timer unprecise. Expected about 1000ms, got", pt1.TimeLeft)
+    }
+}
+
