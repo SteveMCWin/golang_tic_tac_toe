4d5fba5756b3c067ed4d91c75de19d62f37f3e9d
SteveMCWin <stevp003@gmail.com>
2025-09-05 03:54:52 +0200

    small stuff and readme edit

--- data/create_spellfix_user_table.sql
+++ data/create_spellfix_user_table.sql
@@ -1,4 +1,4 @@
-.load ../extensions/spellfix
+.load extensions/spellfix -- has to be extensions/spellfix and not ../extensions/spellfix because of docker
 
 DROP TABLE IF EXISTS spellfix_users;
 CREATE virtual TABLE spellfix_users USING spellfix1;
