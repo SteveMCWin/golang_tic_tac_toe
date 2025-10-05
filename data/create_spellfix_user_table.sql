cbb7e62e7780d1eb2a7848627f842fc39af649a4
SteveMCWin <stevp003@gmail.com>
2025-10-05 18:27:12 +0200

    Experimenting with CI/CD

--- data/create_spellfix_user_table.sql
+++ data/create_spellfix_user_table.sql
@@ -1,4 +1,5 @@
-.load extensions/spellfix -- has to be extensions/spellfix and not ../extensions/spellfix because of docker
+ -- has to be extensions/spellfix and not ../extensions/spellfix because of docker
+.load extensions/spellfix
 
 DROP TABLE IF EXISTS spellfix_users;
 CREATE virtual TABLE spellfix_users USING spellfix1;
